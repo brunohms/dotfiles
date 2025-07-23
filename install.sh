e9da8483fb4228ba8f773d2b027cc79b0b4b5d7f
Bruno Miranda <bruno_h_m_s@hotmail.com>
2025-07-23 17:08:49 -0300

    Add debug code

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 echo "🔧 Aguardando VS Code estar pronto..."
 
-for i in {1..30}; do
+for i in {1..60}; do
+  code --version || echo "Erro no code"
+
   if code --version &> /dev/null; then
     echo "✅ VS Code CLI disponível, instalando extensão..."
     code --install-extension esbenp.prettier-vscode
